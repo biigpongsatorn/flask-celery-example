d8927006d26ea1161c3356a317e88755d9f36910
biigpongsatorn <biig_pongsatorn@hotmail.com>
2024-09-04 13:41:29 +0700

    fix bug re-delivering long task

--- app.py
+++ app.py
@@ -13,23 +13,29 @@ celery.conf.update(app.config)
 celery.conf.update(
     worker_concurrency=3,
     task_default_queue='default',
+    broker_transport_options={"visibility_timeout": float("inf")}
 )
+
+# app.conf.broker_transport_options = { "visibility_timeout": float("inf") }
 
 
 @celery.task()
-def long_task():
+def long_task(n):
     # Simulate a long task
     import time
-    print('long_task hellooooooooo')
-    time.sleep(10)  # Simulates a task that takes 60 seconds
+    print('long_task hellooooooooo', n)
+    time.sleep(30)  # Simulates a task that takes 30 seconds
     return True
 
+numb = 0
 
 @app.route('/api/long_task', methods=['POST'])
 def send_request():
     print('send_request')
+    global numb
+    numb = numb + 1
     # Queue the long task
-    task = long_task.apply_async()
+    task = long_task.apply_async(args=[numb])
     return jsonify({'task_id': task.id, 'status': 'Task has been started'}), 202
 
 
